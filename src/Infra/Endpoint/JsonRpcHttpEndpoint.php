90d45c5126bddb60a966c870870e178e58452a6d
Yoanm <yoanm@users.noreply.github.com>
2018-04-04 08:56:54 +0200

    Add tests

--- src/Infra/Endpoint/JsonRpcHttpEndpoint.php
+++ src/Infra/Endpoint/JsonRpcHttpEndpoint.php
@@ -5,7 +5,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Yoanm\JsonRpcServer\Infra\Endpoint\JsonRpcEndpoint as SDKJsonRpcEndpoint;
 
-class JsonRpcEndpoint
+/**
+ * Class JsonRpcHttpEndpoint
+ */
+class JsonRpcHttpEndpoint
 {
     /** @var SdkJsonRpcEndpoint */
     private $sdkEndpoint;
@@ -17,6 +20,10 @@ class JsonRpcEndpoint
 
     public function index(Request $request) : Response
     {
+        if (Request::METHOD_POST !== $request->getMethod()) {
+            return new Response('A JSON-RPC HTTP call must use POST', Response::HTTP_METHOD_NOT_ALLOWED);
+        }
+
         return new Response(
             $this->sdkEndpoint->index($request->getContent())
         );
